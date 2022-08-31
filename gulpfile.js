f493e7670edc91b7003f290e423342e78e848a9d
Stephen Rinkus <jasperwednesday@gmail.com>
2022-08-31 14:03:25 -0700

    adding cache-buster

--- gulpfile.js
+++ gulpfile.js
@@ -13,27 +13,29 @@ import pump from 'pump';
 import htmlmin from 'gulp-htmlmin';
 import htmlhint from 'gulp-htmlhint';
 import livereload from 'gulp-livereload';
-
-//const babel = import('gulp-babel');
+import cachebust from 'gulp-cache-bust';
 import babel from 'gulp-babel';
 
 const sass = gulpSass(Sass);
 
+//Cache busting
+Gulp.task('cache-bust', () => Gulp.src('./dist/*.html')
+	.pipe(cachebust({ type: 'timestamp' }))
+	.pipe(Gulp.dest('./dist')));
+
 // transpile ES6 to ES5
-Gulp.task('transpile', () => Gulp.src('*.js')
-	.pipe(babel({
-		presets: ['@babel/env'],
-	}))
+Gulp.task('transpile', () => Gulp.src('src/*.js')
+	.pipe(babel({ presets: ['@babel/env'] }))
 	.pipe(Gulp.dest('dist')));
 
 // lint + minify HTML
-Gulp.task('html', () => Gulp.src('*.html')
+Gulp.task('html', () => Gulp.src('src/*.html')
 	.pipe(htmlhint())
 	.pipe(htmlmin({ collapseWhitespace: true }))
 	.pipe(Gulp.dest('dist')));
 
 // lint scss
-Gulp.task('sass-lint', () => Gulp.src('*.scss')
+Gulp.task('sass-lint', () => Gulp.src('src/*.scss')
 	.pipe(sassLint({ configFile: 'config/.sass-lint.yml' }))
 	.pipe(sassLint.format())
 	.pipe(sassLint.failOnError()));
@@ -41,17 +43,17 @@ Gulp.task('sass-lint', () => Gulp.src('*.scss')
 // compile to css and autoprefix styles for cross browser compatibility
 Gulp.task('styles', async (cb) => pump(
 	[
-		Gulp.src('*.scss'),
+		Gulp.src('src/*.scss'),
 		sass().on('error', sass.logError),
 		autoprefixer(),
-		Gulp.dest((f) => f.base),
+		Gulp.dest('dist'),
 		livereload({ start: true }),
 	],
 	cb,
 ));
   
 // lint js 
-Gulp.task('eslint', () => Gulp.src('*.js')
+Gulp.task('eslint', () => Gulp.src('src/*.js')
 	.pipe(eslint())
 	.pipe(eslint.format())
 	.pipe(eslint.failAfterError())); // new function added to check if ESLint has run the fix 
@@ -60,7 +62,7 @@ Gulp.task('eslint', () => Gulp.src('*.js')
 const isFixed = (file) => file.eslint !== null && file.eslint.fixed;
 
 // fix any fixable js errors
-Gulp.task('eslint-fix', () => Gulp.src('*.js')
+Gulp.task('eslint-fix', () => Gulp.src('src/*.js')
 	.pipe(eslint({ fix: true }))
 	.pipe(eslint.format()) 	// if running fix - replace existing file with fixed one 
 	.pipe(GulpIf(isFixed, Gulp.dest('./')))
@@ -69,7 +71,7 @@ Gulp.task('eslint-fix', () => Gulp.src('*.js')
 // uglify the js
 Gulp.task('compress', (cb) => pump(
 	[
-		Gulp.src('*.js'),
+		Gulp.src('src/*.js'),
 		uglify(),
 		Gulp.dest('dist'),
 	],
@@ -79,7 +81,7 @@ Gulp.task('compress', (cb) => pump(
 // watch for any scss changes + live server
 Gulp.task('sass:watch', () => {
 	livereload.listen();
-	Gulp.watch('*.scss', Gulp.series('styles'));
+	Gulp.watch('src/*.scss', Gulp.series('styles'));
 });
 
 ////////////////////////////////////////////////////////////////////////////////
